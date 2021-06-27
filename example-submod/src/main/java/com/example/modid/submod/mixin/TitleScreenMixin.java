81d88911f32b1f3a2cf552249f8b304a807edac8
axieum <7577851+axieum@users.noreply.github.com>
2021-06-27 20:39:55 +1000

    build: adjust Checkstyle import groups

--- example-submod/src/main/java/com/example/modid/submod/mixin/TitleScreenMixin.java
+++ example-submod/src/main/java/com/example/modid/submod/mixin/TitleScreenMixin.java
@@ -4,9 +4,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import static com.example.modid.submod.AnotherExample.LOGGER;
 
 import net.minecraft.client.gui.screen.TitleScreen;
+
+import static com.example.modid.submod.AnotherExample.LOGGER;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin
